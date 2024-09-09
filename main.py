67cdb911a0538e9ab7c0c2ae4918864223dce87a
StarkBak <sstechnologyindtec@gmail.com>
2024-09-09 17:34:12 +0430

    While loop lesson

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-print("Hello, World!")
-print('second line')
-print('edited in github')
+# while loops pass continue break
+# import os
+
+
+# while True:
+#     cmd = input("Enter Your Command: ")
+#     if cmd == 'exit' or cmd == 'q':
+#         exit()
+#     elif cmd == 'cls':
+#         os.system('clear')
+#     elif cmd == 'cd':
+#         os.system('pwd')
+#     elif cmd[0:3] == 'cd ':
+#         if os.path.isdir(cmd[3:]):
+#             os.chdir(cmd[3:])
+#         else:
+#             print('No Such Folder')
+#     elif cmd == 'ls':
+#         os.system('dir')
+#     else:
+#         os.system(cmd)
+
+
+for i in 'SalimBaktash':
+    if i == 'a':
+        continue
+    print(i)
